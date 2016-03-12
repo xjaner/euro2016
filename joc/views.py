6a02575cb6ff1cd2cdba150dc32659f628076920
Xavier Janer <xjaner@gmail.com>
2016-03-12 12:18:13 +0100

    Afegit el login

--- joc/views.py
+++ joc/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index(request):
     return render(request, 'joc/index.html')
 
+@login_required
 def pronostic(request):
     return render(request, 'joc/index.html')
 
+@login_required
 def consulta(request):
     return render(request, 'joc/index.html')
 
+@login_required
 def usuaris(request):
     return render(request, 'joc/index.html')
