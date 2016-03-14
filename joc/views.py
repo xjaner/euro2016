1ef07b39c88d4da617cbf58603d922b660844a41
Xavier Janer <xjaner@gmail.com>
2016-03-14 01:12:52 +0100

    Primer esborrany de template de grup

--- joc/views.py
+++ joc/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from django import forms
+from joc.models import Jugador, Partit, PronosticPartit
+
+
+class GrupForm(forms.ModelForm):
+    gols1 = forms.IntegerField(min_value=0, max_value=8)
+    gols2 = forms.IntegerField(min_value=0, max_value=8)
+    class Meta:
+        model = PronosticPartit
+        fields = ('gols1', 'gols2', 'partit')
 
 @login_required
 def index(request):
@@ -8,7 +18,14 @@ def index(request):
 
 @login_required
 def pronostic(request):
-    return render(request, 'joc/index.html')
+    form = None
+    if request.method == "GET":
+        jugador = Jugador.objects.get(usuari=request.user)
+        partit = Partit.objects.get(pk=1)
+        pronostic_partit = PronosticPartit.objects.create(
+            jugador=jugador, partit=partit)
+        form = GrupForm(instance=pronostic_partit)
+    return render(request, 'joc/grup.html', {'form': form})
 
 @login_required
 def consulta(request):
