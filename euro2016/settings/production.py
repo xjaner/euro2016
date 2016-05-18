6877f0f559c400a07a8d6dae5fdbe8dec178429a
Xavier Janer <xjaner@gmail.com>
2016-05-18 23:56:06 +0200

    Canviat el nom d'usuari de la BD a producció

--- euro2016/settings/production.py
+++ euro2016/settings/production.py
@@ -7,7 +7,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'euro2016',
-        'USER': 'eurocopa',
+        'USER': 'euro2016',
         'PASSWORD': 'franca',
         'HOST': '127.0.0.1',
         'PORT': '3306',
