7bd5961a8b636b1ff00104097c110952d5abdd22
Xavier Janer <xjaner@gmail.com>
2016-05-19 23:49:55 +0200

    Arreglada la pagina d'index

--- joc/views/index.py
+++ joc/views/index.py
@@ -7,7 +7,8 @@ from joc.models import Jugador, PronosticEquipGrup
 @login_required
 def index(request):
     jugador = Jugador.objects.get(usuari=request.user)
-    pronostic_acabat = PronosticEquipGrup.objects.filter(jugador_id=4, posicio__gt=0).count() == 36
+    pronostic_acabat = PronosticEquipGrup.objects.filter(
+        jugador=jugador, posicio__gt=0).count() == 24
     return render(
         request,
         'joc/index.html',
